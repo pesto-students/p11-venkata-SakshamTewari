5ef9d323f8a82c297843256720a9484ade45635f
Saksham Tewari <sakshamtewari3@gmail.com>
2023-05-19 18:18:20 +0530

    Added Assignments

---
+++ Week-5/windowObject.js
@@ -0,0 +1,3 @@
+function displayWindowProperties(){
+    console.log(window.navigator.userAgent , window.screen.width , window.screen.height, window.location.href , window.location.pathname , window.sessionStorage , window.localStorage)
+}
